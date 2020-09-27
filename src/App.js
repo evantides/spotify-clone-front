de48f63ce77e89143f71af0fa5c69a3b1d984c09
Evander Santana <evantually@Evanders-MacBook-Pro.local>
2020-09-27 13:18:02 -0400

    small changes again

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
@@ -11,10 +12,18 @@ function App() {
     updateFormInput({...formInput, ...{[event.target.id]: event.target.value}})
   }
 
+  const handleSelect = event => {
+    updateFormInput({...formInput, ...{[event.target.id]: event.target.value}})
+  }
+
   const handleSubmit = async(event) => {
     event.preventDefault();
+    const terms = formInput.searchTerms.replace(' ', '%20')
     try {
-      const response = await axios.get('')
+      const response = await axios.get(`http://localhost:8888/search/${formInput.searchType}/${terms}`)
+      console.log(response.data)
+    } catch (err) {
+      console.error(err)
     }
   }
 
@@ -22,17 +31,17 @@ function App() {
     <div className="App">
     <form onSubmit={handleSubmit}>
       <input
-        type={'datalist'}
-        value={formInput.searchType}
-        id={"searchType"}
-        onChange={handleChange}
-        />
-        <datalist id={'searchType'}>
-          <option value={"Artist"}/>
-          <option value={"Song Name"}/>
-          <option value={"Album"}/>
-          <option value={"Playlist"}/>
-        </datalist>
+          type={'text'}
+          value={formInput.searchTerms}
+          id={"searchTerms"}
+          onChange={handleChange}
+      />
+        <select id={'searchType'} name={"Select A Search Type"} onChange={handleChange}>
+          <option value={"artist"}>Artist</option>
+          <option value={"track"}>Track</option>
+          <option value={"album"}>Album</option>
+          <option value={"playlist"}>Playlist</option>
+        </select>
       <input type={'submit'} value={"Search"}/>
     </form>
     </div>
